6d2bf23ae67e1b1ce4b899a03e1da1d202f4cd3c
ElyPoli <itsos.poli@gmail.com>
2023-10-18 14:23:28 +0200

    updated footer and header graphics

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,12 +1,61 @@
 <script>
 export default {
+    data() {
+        return {
+            footerLinks: [
+                {
+                    title: "Home",
+                    route: "home",
+                },
+                {
+                    title: "Lista progetti",
+                    route: "projects",
+                }
+            ],
+        };
+    },
 }
 </script>
 
 <template>
-    <footer class="container pt-5 pb-2">Questo è il footer</footer>
+    <div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center my-border-bottom pb-3 mb-3">
+                <li class="nav-item">
+                    <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
+                        <img src="/public/vite.svg" alt="immagine temporanea">
+                    </a>
+                </li>
+                <!-- Link delle pagine -->
+                <li class="nav-item" v-for="link in footerLinks">
+                    <router-link class="nav-link" :class="{ 'active': $route.name === link.route }"
+                        :to="{ name: link.route }">{{ link.title }}</router-link>
+                </li>
+            </ul>
+            <p class="text-center text-body-secondary">&copy; 2023</p>
+        </footer>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
+
+.nav-link {
+    color: $black;
+    transition: all 0.3s;
+    padding: .5rem;
+
+    &:hover {
+        color: $primary-color;
+    }
+}
+
+.my-border-bottom {
+    border-bottom: var(--bs-border-width) var(--bs-border-style) rgba(144, 12, 63, 0.4);
+}
+
+.active {
+    color: $primary-color !important;
+    font-weight: bold;
+}
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -19,16 +19,6 @@ export default {
 
 <template>
     <header>
-        <!-- <nav class="container pt-5 pb-2">
-            <p class="me-2"><router-link :to="{ name: 'login' }">Accedi</router-link></p> -->
-        <!-- Link delle pagine -->
-        <!-- <div class="d-flex align-items-center justify-content-center">
-                <p class="m-2" v-for="link in navLinks">
-                    <router-link :to="{ name: link.route }">{{ link.title }}</router-link>
-                </p>
-            </div>
-        </nav> -->
-
         <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brand" href="#">
